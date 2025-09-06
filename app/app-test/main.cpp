c03fe4809217e80f89399b718e0237b6cc86fa31
faithwh14 <cwh.wen.hao@gmail.com>
2025-09-06 20:32:30 +0800

    feat(app): add cmakelists and init  main app
    Signed-off-by: faithwh14 <cwh.wen.hao@gmail.com>

---
+++ app/app-test/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "THIS IS APP-GUI" << std::endl;
+
+  return 0;
+}
--- run-x86.sh
+++ run-x86.sh
@@ -1,5 +1,5 @@
-rm -rf build-rpi
-mkdir build-rpi
-cd build-rpi
+rm -rf build-x86
+mkdir build-x86
+cd build-x86
 cmake .. -DTOOLCHAIN_FILE=cmake/toolchain-rpi.cmake
 make -j 4
